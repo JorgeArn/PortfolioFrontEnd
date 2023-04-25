5a2f33f9af8a1de29c86a398e1a9bf5db9322e57
Jorge Arnal <jorge.arnal08@gmail.com>
2023-04-24 23:41:22 -0300

    experienciaLab update

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Experiencia } from 'src/app/model/experiencia';
+import { ExperienciaService } from 'src/app/service/experiencia.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-experiencia',
@@ -6,11 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiencia.component.css']
 })
 export class ExperienciaComponent implements OnInit {
-  
-  constructor(){ }
+  expe: Experiencia[] = [];
+
+  constructor(private experienciaService: ExperienciaService, private tokenService: TokenService){ }
+
+  isLogged = false;
 
   ngOnInit(): void {
-    
+    this.cargarExperiencia();
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+
+  }
+
+  cargarExperiencia(): void {
+    this.experienciaService.lista().subscribe(data => {this.expe = data;})
   }
 
 }
